c6f2917e51adacc95d551d2031431cb61737839f
Deliton Junior <deliton@pop-os.localdomain>
2020-10-31 19:16:34 -0300

    implemented horizontal timeline

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,17 @@
-import Head from 'next/head'
-import styles from '../styles/Home.module.css'
-import dbConnect from '../utils/dbConnect'
-import Entry from '../models/entry'
+import Head from "next/head";
+import styles from "../styles/Home.module.css";
+import dbConnect from "../utils/dbConnect";
+import Entry from "../models/entry";
+import { useState } from "react";
+import HorizontalTimeline from "react-horizontal-timeline";
 
 export default function Home({ entries }) {
+  const [value, setvalue] = useState(0);
+  const [previous, setPrevious] = useState(0);
+
+  const dates = entries.map((entry) => {
+    return Date.now();
+  });
   return (
     <div className={styles.container}>
       <Head>
@@ -16,39 +24,51 @@ export default function Home({ entries }) {
           Welcome to <a href="https://wow.loreportal.com">Lore Portal!</a>
         </h1>
 
-        {entries.map(entry => {
-         return ( <div>
-            <h3>{entry.title}</h3>
-            <p>{entry.text}</p>
-            <a href={entry.video}>Watch Video</a>
-          </div>)
-        })}
+        {/* {entries.map((entry) => {
+          return (
+            <div>
+              <h3>{entry.title}</h3>
+              <p>{entry.text}</p>
+              <a href={entry.video}>Watch Video</a>
+            </div>
+          );
+        })} */}
+
+        <div>
+          {/* Bounding box for the Timeline */}
+          <div>
+            <h3>{entries[value].title}</h3>
+            <p>{entries[value].text}</p>
+            <a href={entries[value].video}>Watch Video</a>
+          </div>
+          <div style={{ width: "100%", height: "150px", margin: "0 auto" }}>
+            <HorizontalTimeline
+              index={value}
+              indexClick={(index) => {
+                setvalue(index);
+                setPrevious(value);
+              }}
+              values={dates}
+            />
+          </div>
+        </div>
       </main>
 
-      <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
-        </a>
-      </footer>
+      <footer className={styles.footer}></footer>
     </div>
-  )
+  );
 }
 
 export async function getServerSideProps() {
-  await dbConnect()
+  await dbConnect();
 
   /* find all the data in our database */
-  const result = await Entry.find({})
+  const result = await Entry.find({});
   const entries = result.map((doc) => {
-    const entry = doc.toObject()
-    entry._id = entry._id.toString()
-    return entry
-  })
+    const entry = doc.toObject();
+    entry._id = entry._id.toString();
+    return entry;
+  });
 
-  return { props: { entries: entries } }
+  return { props: { entries: entries } };
 }
